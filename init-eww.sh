118768b85ac8d81aaf677256825082ef05946a6e
Jakub Celmer <astriblast@gmail.com>
2025-11-07 19:51:38 +0000

    autocommit: 2025-11-07T19:51:38+00:00

--- init-eww.sh
+++ init-eww.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/env bash
+# źródłowane z ~/.bashrc; bez "exit"
 eww::init() {
   [[ $- == *i* ]] || return 0
   export EWW_ROOT="${EWW_ROOT:-/git/eww}"
   local LIB="$EWW_ROOT/lib/bash/baner.sh"
 
   : "${EWW_CD_ROOT:=1}"
-  [[ "$EWW_CD_ROOT" != 0 && -d "$EWW_ROOT" && "$PWD" != "$EWW_ROOT" ]] && cd "$EWW_ROOT" || :
+  if [[ "$EWW_CD_ROOT" != 0 && -d "$EWW_ROOT" && "$PWD" != "$EWW_ROOT" ]]; then
+    cd "$EWW_ROOT" || true
+  fi
 
   : "${EWW_BANNER_PREFIX:=Miłego dnia}"
   : "${EWW_BANNER_BOTTOM_LEFT:=E-Waste Workshop}"
   : "${EWW_BANNER_BOTTOM_RIGHT:=www.E-WasteWorkshop.co.uk}"
 
-  [[ -r "$LIB" ]] && . "$LIB"
-
-  if ! declare -F eww_banner >/dev/null; then
-    eww_banner() { printf "EWW minimal banner\n"; }
+  if [[ -r "$LIB" ]]; then
+    # shellcheck source=/git/eww/lib/bash/baner.sh
+    . "$LIB"
   fi
 
-  export EWW_INIT_OK=1
+  # raz na sesję
   if [[ -z "${EWW_BANNER_SHOWN-}" ]]; then
-    export EWW_BANNER_SHOWN=1
-    eww_banner
+    export EWW_BANNER_SHOWN=1 EWW_INIT_OK=1
+    if declare -F eww_banner >/dev/null; then
+      eww_banner
+    fi
+  else
+    export EWW_INIT_OK=1
   fi
 }
 eww::init
