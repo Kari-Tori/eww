c51e6eea454d2676a1523753db03348f1038021e
Jakub Celmer <astriblast@gmail.com>
2025-11-07 17:59:40 +0000

    autocommit: 2025-11-07T17:59:40+00:00

--- init-eww.sh
+++ init-eww.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# SOURCOWANY z ~/.bashrc; bezpieczny także przy uruchomieniu
+# musi być SOURCOWANY z ~/.bashrc; bezpieczny także przy uruchomieniu
 
 eww::init() {
   case "$-" in *i*) ;; *) return 0 ;; esac
@@ -7,31 +7,30 @@ eww::init() {
   export EWW_ROOT="${EWW_ROOT:-/git/eww}"
   local LIB_BANER="${EWW_ROOT}/lib/bash/baner.sh"
 
-  # auto-cd działa zawsze
   : "${EWW_CD_ROOT:=1}"
   if [[ "$EWW_CD_ROOT" != "0" && -d "$EWW_ROOT" && "${PWD:-}" != "$EWW_ROOT" ]]; then
     cd "$EWW_ROOT" || :
   fi
 
-  # domyślne napisy
   : "${EWW_BANNER_PREFIX:=Miłego dnia}"
   : "${EWW_BANNER_BOTTOM_LEFT:=E-Waste Workshop}"
   : "${EWW_BANNER_BOTTOM_RIGHT:=www.E-WasteWorkshop.co.uk}"
 
-  # pokaż baner tylko raz na sesję
-  local SHOW=1
+  local show=1
   if [[ -n "${EWW_BANNER_SHOWN-}" ]]; then
-    SHOW=0
+    show=0
   else
     export EWW_BANNER_SHOWN=1
   fi
 
-  if [[ $SHOW -eq 1 ]]; then
+  if [[ $show -eq 1 ]]; then
     if [[ -r "$LIB_BANER" ]]; then
       # shellcheck source=/git/eww/lib/bash/baner.sh
       . "$LIB_BANER"
       export EWW_INIT_OK=1
-      type eww::baner >/dev/null 2>&1 && eww::baner
+      if type eww::baner >/dev/null 2>&1; then
+        eww::baner
+      fi
     else
       printf "\n╭─ %s@%s • %s\n" "$(id -un)" "$(hostname)" "$(date '+%F %T')"
       printf "╰─ %s • %s • repo:%s • cfg:%s [MIN]\n\n" \
@@ -39,11 +38,6 @@ eww::init() {
         "$EWW_ROOT" "$EWW_ROOT/init-eww.sh"
     fi
   fi
-  return 0
 }
 
-# sourced → return; uruchomiony → exit 0
-else
-  eww::init; exit 0
-fi
 eww::init
